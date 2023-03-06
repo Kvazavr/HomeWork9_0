d2dc73fb872f7735120d005e8cff52fa7fca0d54
Elena Nekrasova <kvazavr@yandex.ru>
2023-03-06 13:18:50 +0400

    Initial commit

---
+++ src/main/java/ru/netology/Radio.java
@@ -0,0 +1,46 @@
+package ru.netology;
+
+public class Radio {
+    public int numberOfStation;
+    public int stationCount = 10;
+    public int currentVolume;
+    public int maxVolume = 100;
+    public int minVolume = 0;
+
+    public void setNumberOfStation(int newNumberOfStation) {
+
+        if (numberOfStation > 9) {
+            return;
+        }
+        numberOfStation = newNumberOfStation;
+    }
+
+    public void next() {
+        numberOfStation++;
+        if (numberOfStation > stationCount - 1) {
+            numberOfStation = 0;
+        }
+
+    }
+
+    public void prev() {
+        numberOfStation--;
+        if (numberOfStation < 0) {
+            numberOfStation = stationCount - 1;
+        }
+    }
+
+    public void increaseVolume() {
+        currentVolume++;
+        if (currentVolume > maxVolume) {
+            currentVolume = maxVolume;
+        }
+    }
+
+    public void decreaseVolume() {
+        currentVolume--;
+        if (currentVolume < minVolume) {
+            currentVolume = minVolume;
+        }
+    }
+}
---
+++ src/test/java/ru/netology/RadioTest.java
@@ -0,0 +1,12 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+public class RadioTest {
+    @Test
+public void numberOfStation () {
+    Radio radio = new Radio();
+    radio.setNumberOfStation(8);
+    int expected = 8;
+    int actual = 8;
+    }
+}
