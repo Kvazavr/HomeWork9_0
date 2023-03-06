6418e3063439bf5b576f2b3577dbe5fb911eeff2
Elena Nekrasova <kvazavr@yandex.ru>
2023-03-06 13:48:50 +0400

    Modified Test

--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -1,12 +1,13 @@
 package ru.netology;
 
 import org.junit.jupiter.api.Test;
+
 public class RadioTest {
     @Test
-public void numberOfStation () {
-    Radio radio = new Radio();
-    radio.setNumberOfStation(8);
-    int expected = 8;
-    int actual = 8;
+    public void numberOfStation() {
+        Radio radio = new Radio();
+        radio.setNumberOfStation(8);
+        int expected = 8;
+        int actual = 8;
     }
 }
